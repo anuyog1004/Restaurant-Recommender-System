a7120d03dd49eb894db778f9e9c60c0ad5ea0cb1
anuyog1004 <anuyog1004@gmail.com>
2017-03-29 22:30:09 +0530

    Initial Commit

--- BuildRestaurantData.py
+++ BuildRestaurantData.py
@@ -1,6 +1,7 @@
 import requests
 import sqlite3
 import json
+import config
 
 conn=sqlite3.connect('restaurants_data.sqlite')
 conn.text_factory = str
@@ -21,16 +22,16 @@ start_index = 0
 for category in category_ids:
 	print "Doing for category " , category
 	while(True):
-		r = requests.get(request_url,headers={"user-key":"576b8aee91166bc426d076945be5d8fa"},params={"entity_id":1,"entity_type":"city","establishment_type":category,"start":start_index})
+		r = requests.get(request_url,headers={"user-key":config.API_KEY},params={"entity_id":1,"entity_type":"city","establishment_type":category,"start":start_index})
 		js = json.loads(r.content)
 		if start_index==js["results_found"] or start_index>=100 or js["results_shown"]==0:
 			start_index=0
 			break
 		for i in range(0,js["results_shown"]):
 			print js["restaurants"][i]["restaurant"]["name"] 
-			cur.execute('''
-                    INSERT INTO RESTAURANTS (res_id,res_name) VALUES (?,?)''',(js["restaurants"][i]["restaurant"]["R"]["res_id"],js["restaurants"][i]["restaurant"]["name"] + ", " + js["restaurants"][i]["restaurant"]["location"]["locality"]))
-			conn.commit()
+			# cur.execute('''
+   #                  INSERT INTO RESTAURANTS (res_id,res_name) VALUES (?,?)''',(js["restaurants"][i]["restaurant"]["R"]["res_id"],js["restaurants"][i]["restaurant"]["name"] + ", " + js["restaurants"][i]["restaurant"]["location"]["locality"]))
+			# conn.commit()
 
 
 		start_index += js["results_shown"] 
